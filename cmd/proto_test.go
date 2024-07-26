f1d316ee20f3c32c096cbfce2dc57fe8f80da4cd
alenduan <alenduan@tencent.com>
2024-07-26 21:00:56 +0800

    proto

--- cmd/proto_test.go
+++ cmd/proto_test.go
@@ -3,22 +3,27 @@ package main
 import (
 	"fmt"
 	"github.com/yaoguangduan/reskeeper/pbgen"
+	"github.com/yaoguangduan/reskeeper/resproto"
 	"google.golang.org/protobuf/proto"
-	"os"
+	"google.golang.org/protobuf/types/descriptorpb"
 	"testing"
 )
 
 func TestGene(t *testing.T) {
-	file, err := os.ReadFile("..\\data\\Zoo_full.bin")
-	if err != nil {
-		panic(err)
-	}
+	//file, err := os.ReadFile("..\\data\\Zoo_full.bin")
+	//if err != nil {
+	//	panic(err)
+	//}
 	zooTable := pbgen.ZooTable{}
-	err = proto.Unmarshal(file, &zooTable)
-	for _, zoo := range zooTable.Zoos {
-		for _, b := range zoo.Borrows {
-			fmt.Println(b)
-		}
-	}
-	fmt.Println(&zooTable)
+	//err = proto.Unmarshal(file, &zooTable)
+	//for _, zoo := range zooTable.Zoos {
+	//	for _, b := range zoo.Borrows {
+	//		fmt.Println(b)
+	//	}
+	//}
+	//fmt.Println(&zooTable)
+	desc := zooTable.ProtoReflect().Descriptor()
+	mo := desc.Options().(*descriptorpb.MessageOptions)
+	e := proto.GetExtension(mo, resproto.E_ExcelAndSheetName)
+	fmt.Println(e)
 }
